9c28ba6cd0d6d640ad315017ce005c4d1e0f7bff
ejwood003 <ejwood@byu.edu>
2026-03-25 18:11:06 -0600

    Fixed program.cs imports

--- backend/flourishbackend/flourishbackend/Program.cs
+++ backend/flourishbackend/flourishbackend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using flourishbackend.Data;
+using flourishbackend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
